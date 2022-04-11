4e4266d7fe6a2715cfd50337343ab21c798e7db2
andrea_bianchi24 <andrea.bianchi24@studio.unibo.it>
2022-04-11 15:21:17 +0200

    defined object student that creates a new Student with apply def: the object also overrides and defines enrolling method, which takes a series of elements which are then added to the list of courses that the student participates to, courses method to get the name of every course that the student partecipates to, and hasTeacher method, which searches if the student partecipates to a course of a specific teacher. defined object course that creates a new Course with apply def.

--- src/main/scala/u04lab/code/Student.scala
+++ src/main/scala/u04lab/code/Student.scala
@@ -1,11 +1,11 @@
 package u04lab.code
 
-import List.*
+import u04lab.code.List.*
 
 trait Student:
   def name: String
   def year: Int
-  def enrolling(course: Course): Unit // the student participates to a Course
+  def enrolling(course: Course*): Unit // the student participates to a Course
   def courses: List[String] // names of course the student participates to
   def hasTeacher(teacher: String): Boolean // is the student participating to a course of this teacher?
 
@@ -14,10 +14,25 @@ trait Course:
   def teacher: String
 
 object Student:
-  def apply(name: String, year: Int = 2017): Student = ???
+  def apply(name: String, year: Int = 2017): Student =
+    StudentImpl(name, year)
+  case class StudentImpl(override val name: String, override val year: Int) extends Student:
+    private var studentCourses: List[Course] = List.Nil()
+
+    override def enrolling(course: Course*): Unit =
+      course.foreach(el => studentCourses = append(studentCourses, List.Cons(el, List.Nil())))
+
+    override def courses: List[String] =
+      List.map(studentCourses)(c => c.name)
+
+    override def hasTeacher(teacher: String): Boolean =
+      contains(List.map(studentCourses)(c => c.teacher), teacher)
+
 
 object Course:
-  def apply(name: String, teacher: String): Course = ???
+  def apply(name: String, teacher: String): Course =
+    CourseImpl(name, teacher)
+  case class CourseImpl(override val name: String, override val teacher: String) extends Course
 
 @main def checkStudents(): Unit =
   val cPPS = Course("PPS", "Viroli")
@@ -26,16 +41,24 @@ object Course:
   val s1 = Student("mario", 2015)
   val s2 = Student("gino", 2016)
   val s3 = Student("rino") // defaults to 2017
+  val s4 = Student("giacomo")
+  val allCourses = List.Cons(cPPS, List.Cons(cPCD, List.Cons(cSDR, List.Nil())))
+  /*
+  allCourses match
+    case sameTeacher(t) => println(s"$allCourses have same teacher $t")
+    case _ => println(s"$allCourses have different teachers")*/
   s1.enrolling(cPPS)
   s1.enrolling(cPCD)
   s2.enrolling(cPPS)
   s3.enrolling(cPPS)
   s3.enrolling(cPCD)
   s3.enrolling(cSDR)
+  s4.enrolling(cSDR,cPPS,cPCD)
   println(
     (s1.courses, s2.courses, s3.courses)
   ) // (Cons(PCD,Cons(PPS,Nil())),Cons(PPS,Nil()),Cons(SDR,Cons(PCD,Cons(PPS,Nil()))))
   println(s1.hasTeacher("Ricci")) // true
+  println(s4.courses)
 
 /** Hints:
   *   - simply implement Course, e.g. with a case class
