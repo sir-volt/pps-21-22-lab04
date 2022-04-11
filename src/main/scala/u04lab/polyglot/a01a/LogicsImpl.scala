95a1fc95d36bb0e7697643cabf88c396df46738f
andrea_bianchi24 <andrea.bianchi24@studio.unibo.it>
2022-04-11 15:18:07 +0200

    Added some private fields for LogicsImpl.scala

--- src/main/scala/u04lab/polyglot/a01a/LogicsImpl.scala
+++ src/main/scala/u04lab/polyglot/a01a/LogicsImpl.scala
@@ -1,8 +1,24 @@
 package u04lab.polyglot.a01a
 import Logics.*
+import scala.util.Random
+import u04lab.code.List
+import u04lab.code.List
 
 /** solution and descriptions at https://bitbucket.org/mviroli/oop2019-esami/src/master/a01a/sol2/ */
 class LogicsImpl(private val size: Int, private val boat: Int) extends Logics:
 
-  def hit(row: Int, col: Int) =
+  private val failures = 5
+  private val random = Random()
+  private val boatRow = random.nextInt(size)
+  private val boatLeftCol = random.nextInt(size - boat + 1)
+  private val pushedButtons: List[(Int, Int)] = List.Nil()
+
+
+
+  override def hit(row: Int, col: Int) =
     Result.HIT
+    /*(row, col) match
+    case (row, col) if(row == boatRow && col >= boatLeftCol
+      && col < (boatLeftCol + size)) => append()*/
+
+
