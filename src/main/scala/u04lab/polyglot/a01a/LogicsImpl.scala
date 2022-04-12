396efce0210cd3ec3602dd84902f71fe05363edf
andrea_bianchi24 <andrea.bianchi24@studio.unibo.it>
2022-04-12 15:13:15 +0200

    Completed LogicsImpl.scala in order to work like the java code

--- src/main/scala/u04lab/polyglot/a01a/LogicsImpl.scala
+++ src/main/scala/u04lab/polyglot/a01a/LogicsImpl.scala
@@ -1,7 +1,8 @@
 package u04lab.polyglot.a01a
 import Logics.*
 import scala.util.Random
-import u04lab.code.List
+
+import u04lab.code.List.*
 import u04lab.code.List
 
 /** solution and descriptions at https://bitbucket.org/mviroli/oop2019-esami/src/master/a01a/sol2/ */
@@ -11,12 +12,23 @@ class LogicsImpl(private val size: Int, private val boat: Int) extends Logics:
   private val random = Random()
   private val boatRow = random.nextInt(size)
   private val boatLeftCol = random.nextInt(size - boat + 1)
-  private val pushedButtons: List[(Int, Int)] = List.Nil()
+  private var pushedButtons: List[(Int, Int)] = List.Nil()
+  private var tries = 0
 
 
 
   override def hit(row: Int, col: Int) =
-    Result.HIT
+    if(row == boatRow && col >= boatLeftCol && col <= (boatLeftCol + size)) then
+      pushedButtons = append(pushedButtons, List.Cons((row, col), Nil()))
+      length(pushedButtons) match
+        case n if (n == boat) => Result.WON
+        case _ => Result.HIT
+    else
+      tries = tries.+(1)
+      tries match
+        case n if (n == failures) => Result.LOST
+        case _ => Result.MISS
+
     /*(row, col) match
     case (row, col) if(row == boatRow && col >= boatLeftCol
       && col < (boatLeftCol + size)) => append()*/
