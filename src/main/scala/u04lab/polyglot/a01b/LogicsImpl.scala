e81e1eaf15eee76be8adb749d143841c7e458b3d
andrea_bianchi24 <andrea.bianchi24@studio.unibo.it>
2022-04-21 09:04:51 +0200

    little modification to LogicsImpl.scala

--- src/main/scala/u04lab/polyglot/a01b/LogicsImpl.scala
+++ src/main/scala/u04lab/polyglot/a01b/LogicsImpl.scala
@@ -6,6 +6,8 @@ import u04lab.code.Option.*
 
 /** solution and descriptions at https://bitbucket.org/mviroli/oop2019-esami/src/master/a01b/sol2/ */
 class LogicsImpl(private val size: Int, private val mines: Int) extends Logics:
+  
+  
   def hit(x: Int, y: Int): java.util.Optional[Integer] =
     OptionToOptional(None()) // Option => Optional converter
 
